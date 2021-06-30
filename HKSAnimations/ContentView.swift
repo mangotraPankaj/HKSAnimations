11a1c0b02741dd4fb1517b27ea908f6c33bdc3c8
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-30 11:10:16 +0530

    Initial Commit

---
+++ HKSAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HKSAnimations
+//
+//  Created by Pankaj Mangotra on 30/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKSAnimations/HKSAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKSAnimationsApp.swift
+//  HKSAnimations
+//
+//  Created by Pankaj Mangotra on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HKSAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
