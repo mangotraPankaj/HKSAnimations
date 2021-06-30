7eeb1ff54a67499d66d26cf91a8e21c0e6898f93
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-30 12:08:50 +0530

    Creating animations on state change

--- HKSAnimations/ContentView.swift
+++ HKSAnimations/ContentView.swift
@@ -11,25 +11,23 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     
     var body: some View {
-        Button(action: {
-            //self.animationAmount += 0.2
-        }, label: {
-            Text("Tap Me")
-        })
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(.yellow)
-        .clipShape(Circle())
-        .overlay(Circle()
-                    .stroke(Color.blue)
-                    .scaleEffect(animationAmount)
-                    .opacity(Double(2 - animationAmount))
-                    .animation(Animation.easeOut(duration: 1)
-                                .repeatForever(autoreverses: false)
-                    )
-        )
-        .onAppear {
-            self.animationAmount = 2
+        print(animationAmount)
+       return VStack {
+            Stepper("Scale Amount", value: $animationAmount.animation(
+                        Animation.easeInOut(duration: 1)
+                            .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button(action: {
+                self.animationAmount += 1
+            }, label: {
+                Text("Tap Me")
+            })
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
         
     }
