419ed828148b01ee29ec139c6e871bbf81c30089
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-30 12:17:12 +0530

    Creating 3d rotation effects using explicit animations

--- HKSAnimations/ContentView.swift
+++ HKSAnimations/ContentView.swift
@@ -8,27 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        print(animationAmount)
-       return VStack {
-            Stepper("Scale Amount", value: $animationAmount.animation(
-                        Animation.easeInOut(duration: 1)
-                            .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-            Button(action: {
-                self.animationAmount += 1
-            }, label: {
-                Text("Tap Me")
-            })
-            .padding(40)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
-        }
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }.padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 1.0, y: 0.0, z: 0.0)
+        )
         
     }
 }
