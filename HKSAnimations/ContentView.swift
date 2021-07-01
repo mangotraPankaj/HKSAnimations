7388c7dae3289c5702a513ff03cfebfe63cbd2e5
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-01 12:01:43 +0530

    Transitioning of views based on the conditions

--- HKSAnimations/ContentView.swift
+++ HKSAnimations/ContentView.swift
@@ -9,23 +9,19 @@ struct ContentView: View {
     @State private var enabled = false
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(self.letters[num]))
-                    .padding(10)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(Animation.default .delay(Double(num) / 20))
-            }
-        }.gesture(
-            DragGesture()
-                .onChanged {self.dragAmount = $0.translation}
-                .onEnded {_ in
-                    self.dragAmount = .zero
+        VStack {
+            Button("Tap me") {
+                withAnimation {
                     self.enabled.toggle()
                 }
-        )
+            }
+            if(enabled) {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: 100, alignment: .center)
+                .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
     }
 }
 
